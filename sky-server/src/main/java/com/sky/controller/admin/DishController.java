a64de6814d705c6a1b426b94c7617773e7426de0
LIHANYU <LHY1715446406@QQ.COM>
2026-06-04 20:48:50 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 //菜品管理
 @RestController
 @RequestMapping("/admin/dish")
@@ -36,5 +38,14 @@ public class DishController {
         log.info("菜品分页查询:{}",dishPageQueryDTO);
         PageResult pageResult=dishService.pageQuery(dishPageQueryDTO);
         return Result.success(pageResult);
+    }
+
+    //菜品批量删除
+    @DeleteMapping
+    @ApiOperation("菜品删除")
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("删除菜品:{}",ids);
+        dishService.deleteBatch(ids);
+        return Result.success(ids);
     }
 }
