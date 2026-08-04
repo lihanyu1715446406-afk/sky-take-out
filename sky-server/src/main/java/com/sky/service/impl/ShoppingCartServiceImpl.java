34ad18e85b8e63a1e7e9889f097a16d42ecf7a59
LIHANYU <LHY1715446406@QQ.COM>
2026-08-04 14:01:23 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -68,5 +68,17 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCart.setCreateTime(LocalDateTime.now());
             shoppingCartMapper.insert(shoppingCart);
         }
+
+
+    }
+    //查看购物车
+    public List<ShoppingCart> showShoppingCart(){
+        //获取到当前微信用户的id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart=ShoppingCart.builder()
+                .userId(userId)
+                .build();
+        List<ShoppingCart> list=shoppingCartMapper.list(shoppingCart);
+        return list;
     }
 }
