9c77d6fa30e32c6165268da2e9b6df30b4869cf5
LIHANYU <LHY1715446406@QQ.COM>
2026-09-20 16:14:10 +0800

    用户数据统计功能代码开发

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,12 +1,16 @@
 package com.sky.service;
 
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 
 import java.time.LocalDate;
 
 public interface ReportService {
     //统计指定时间区间内的营业额数据
     TurnoverReportVO getTurnoverStatistics(LocalDate begin,LocalDate end);
+
+    //统计指定时间内的用户数据
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
 
 
 }
